73408dbe398fb5798bf1f27d63c113dc8d652034
Nikhil G Singh <90859496+NIKHILGSINGH131@users.noreply.github.com>
2023-11-09 16:32:47 +0530

    Update NewsApp.js

--- src/12. forms and hooks/NewsApp.js
+++ src/12. forms and hooks/NewsApp.js
@@ -5,7 +5,7 @@ import "./NewsApp.css";
 function NewsApp() {
   const apiKey = "e0e74ed7ee9b4923bd03e9109fbf8fb2";
   const [key, setKey] = useState("tesla");
-   const apiUrl = `https://newsapi.org/v2/everything?q=${key}&from=2023-10-09&sortBy=publishedAt&apiKey=${apiKey}`;
+   https://newsapi.org/v2/everything?q=tesla&from=2023-10-09&sortBy=publishedAt&apiKey=e0e74ed7ee9b4923bd03e9109fbf8fb2
   const [newsList, setNewsList] = useState([]);
   const keyInputref = useRef(null);
   
